3492428d517bf26f467093a66c1f4e0ef7e10013
hgraham <hgraham@redhat.com>
2015-09-23 16:05:10 -0400

    fixing problem where script failed on the first line of the get rc output

--- oc_delete.sh
+++ oc_delete.sh
@@ -3,7 +3,7 @@
 oc login
 for each in "templates" "bc" "builds" "routes" "pods" "dc" "se" "is" "rc";
 	    do
-		for line in `oc get $each | awk '{ print $1 }' | grep -v "NAME"`;
+		for line in `oc get $each | awk '{ print $1 }' | grep -v "NAME" | grep -v "CONTROLLER"`;
 		do
 		    oc delete $each $line;
 		done;
