963d84a30e3a14e3a69bffeea111de0928815abe
hgraham <hgraham@redhat.com>
2015-12-18 14:25:06 -0500

    added svc for the newer versions of openshift

--- oc_delete.sh
+++ oc_delete.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 oc login
-for each in "templates" "bc" "builds" "routes" "pods" "dc" "se" "is" "rc";
+for each in "templates" "bc" "builds" "routes" "pods" "dc" "se" "is" "rc" "svc";
 	    do
 		for line in `oc get $each | awk '{ print $1 }' | grep -v "NAME" | grep -v "CONTROLLER"`;
 		do
